62fb5858b6d10702a2539c4f1148316c9ce64b53
monse <monsemendez@2806-106e-0024-14c8-de58-0939-e349-84d7.ipv6.infinitum.net.mx>
2023-11-07 22:22:08 -0600

    first commit

---
+++ src/Arreglos.java
@@ -0,0 +1,63 @@
+public class Arreglos{
+    /**
+     * Encuentra el número más repetido dentro de un arreglo.
+     * @param numeros un arreglo de numeros enteros.
+     * @return el entero más repetido del arreglo.
+     */
+    public int masRepetido(int[] numeros){
+        
+    }
+    
+
+    /**
+     * Regresa el espejo de una matriz.
+     * @param arreglo el arreglo a calcular su espejo.
+     * @return Un arreglo con la representación de la
+     * matriz espejo del parámetro.
+     */
+    public int[][] matrizEspejo(int[][] arreglo){
+
+    }
+
+
+    /**
+     * Crea una cadena a partir de un arreglo bidimensional de caracteres
+     * Cada subarreglo será una palabra en la frase.
+     * Cada palabra estará separada por un espacio.
+     * @param frase el arreglo con la frase.
+     * @return la frase formada a partir de caracteres.
+     */
+    public String construyeFrase(char[][] frase){
+
+    }
+
+    /**
+     * Permite saber si todos los elementos de un arreglo están contenidos en otro.
+     * @param contenido un arreglo de numeros enteros
+     * @param contenedor un arreglo donde verificar si el arreglo
+     * contiene todos los elementos de contenido.
+     * @return true si contiene todos los elementos, false en otro caso.
+     */
+    public boolean contenido(int[] contenido, int[] contenedor){
+
+    }
+
+    /**
+    * Elimina los elementos repetidos de un arreglo.
+    * @param arreglo el arreglo del cual eliminar repetidos.
+    * @return un arreglo sin elementos repetidos.
+    */
+    public int[] quitaRepetidos(int[] arreglo){
+
+    }
+
+    /**
+     * Construye tantos niveles del triángulo de Pascal como se soliciten.
+     * @param n la cantidad de niveles del triangulo a construir.
+     * @return un arreglo con la representación del tríangulo de pascal
+     * con n niveles.
+     */
+    public int[][] pascal(int n){
+
+    }
+}
